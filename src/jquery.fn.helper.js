8068be5e1eacb0ed21988a9bc070e3f59204f6a1
Philipp <philipp.nikolajev@suomenpt.fi>
2019-01-12 15:33:48 +0200

    slideDownUp() created

--- src/jquery.fn.helper.js
+++ src/jquery.fn.helper.js
@@ -1,7 +1,41 @@
 (function (jQuery) {
 
-    jQuery.fn.isShown = function(){
-        return jQuery(this).css('display') !== 'none';
+    // @todo Move to js.helper later
+    function setOption(optionTitle, options, defaultOptions){
+        return options[optionTitle] === undefined ? defaultOptions[optionTitle] : options[optionTitle];
+    }
+
+    jQuery.fn.isShown = function () {
+        var result = true;
+
+        this.each(function () {
+            if (jQuery(this).css('display') === 'none') {
+                result = false;
+            }
+        });
+
+        return result;
+    };
+
+    jQuery.fn.slideDownUp = function (options) {
+
+        if (options === undefined) {
+            options = {};
+        }
+
+        var defaultOptions = {
+            slideDown: 1000,
+            delay: 1000,
+            slideUp: 1000
+        };
+
+        return this.each(function () {
+            jQuery(this)
+                .finish()
+                .slideDown(setOption('slideDown', options, defaultOptions))
+                .delay(setOption('delay', options, defaultOptions))
+                .slideUp(setOption('slideUp', options, defaultOptions));
+        });
     };
 
 }(jQuery));
