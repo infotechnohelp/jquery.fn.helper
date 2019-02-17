ca0745fcb4988c2aa695eb7cc4bad68fc56bd4e5
Philipp <philipp.nikolajev@suomenpt.fi>
2019-02-17 13:02:01 +0200

    scrollEnd(), resizeEnd(), mousemoveEnd(), isOnScreen()

--- src/jquery.fn.helper.js
+++ src/jquery.fn.helper.js
@@ -35,4 +35,63 @@
         });
     };
 
+    jQuery.fn.scrollEnd = function (callback, timeout) {
+
+        $(this).scroll(function () {
+            var $this = $(this);
+            if ($this.data('scrollTimeout')) {
+                clearTimeout($this.data('scrollTimeout'));
+            }
+            $this.data('scrollTimeout', setTimeout(callback, timeout));
+        });
+
+    };
+
+    jQuery.fn.resizeEnd = function (callback, timeout) {
+
+        $(this).resize(function () {
+            var $this = $(this);
+            if ($this.data('resizeTimeout')) {
+                clearTimeout($this.data('resizeTimeout'));
+            }
+            $this.data('resizeTimeout', setTimeout(callback, timeout));
+        });
+
+    };
+
+    jQuery.fn.mousemoveEnd = function (callback, timeout) {
+
+        $(this).mousemove(function () {
+            var $this = $(this);
+            if ($this.data('mousemoveTimeout')) {
+                clearTimeout($this.data('mousemoveTimeout'));
+            }
+            $this.data('mousemoveTimeout', setTimeout(callback, timeout));
+        });
+
+    };
+    
+    jQuery.fn.isOnScreen = function (options) {
+
+        var win = $(window);
+
+        var viewport = {
+            top: win.scrollTop(),
+            left: win.scrollLeft()
+        };
+        viewport.right = viewport.left + win.width();
+        viewport.bottom = viewport.top + win.height();
+
+        if (options.marginBottom !== undefined) {
+            viewport.bottom = viewport.bottom - options.marginBottom;
+        }
+
+        var bounds = this.offset();
+        bounds.right = bounds.left + this.outerWidth();
+        bounds.bottom = bounds.top + this.outerHeight();
+
+        return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+    };
+
 }(jQuery));
