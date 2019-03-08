ad86408670ace05cbb177c2f0ab51bdebf0d2a89
Philipp <philipp.nikolajev@suomenpt.fi>
2019-03-08 18:04:20 +0200

    Ready to publish 1.2.1

--- src/jquery.fn.helper.js
+++ src/jquery.fn.helper.js
@@ -1,5 +1,7 @@
 (function (jQuery) {
 
+    var js = new Helper();
+
     jQuery.fn.isShown = function () {
         var result = true;
 
@@ -35,42 +37,37 @@
         });
     };
 
+
     jQuery.fn.scrollEnd = function (callback, timeout) {
 
+        var scrollTimeout = null;
         $(this).scroll(function () {
-            var $this = $(this);
-            if ($this.data('scrollTimeout')) {
-                clearTimeout($this.data('scrollTimeout'));
-            }
-            $this.data('scrollTimeout', setTimeout(callback, timeout));
+            clearTimeout(scrollTimeout);
+            scrollTimeout = setTimeout(callback, timeout);
         });
 
     };
 
     jQuery.fn.resizeEnd = function (callback, timeout) {
 
+        var resizeTimeout = null;
         $(this).resize(function () {
-            var $this = $(this);
-            if ($this.data('resizeTimeout')) {
-                clearTimeout($this.data('resizeTimeout'));
-            }
-            $this.data('resizeTimeout', setTimeout(callback, timeout));
+            clearTimeout(resizeTimeout);
+            resizeTimeout = setTimeout(callback, timeout);
         });
 
     };
 
     jQuery.fn.mousemoveEnd = function (callback, timeout) {
 
+        var mousemoveTimeout = null;
         $(this).mousemove(function () {
-            var $this = $(this);
-            if ($this.data('mousemoveTimeout')) {
-                clearTimeout($this.data('mousemoveTimeout'));
-            }
-            $this.data('mousemoveTimeout', setTimeout(callback, timeout));
+            clearTimeout(mousemoveTimeout);
+            mousemoveTimeout = setTimeout(callback, timeout);
         });
 
     };
-    
+
     jQuery.fn.isOnScreen = function (options) {
 
         var win = $(window);
@@ -79,14 +76,16 @@
             top: win.scrollTop(),
             left: win.scrollLeft()
         };
+
         viewport.right = viewport.left + win.width();
-        viewport.bottom = viewport.top + win.height();
+        viewport.bottom = viewport.top + win.outerHeight();
 
         if (options.marginBottom !== undefined) {
             viewport.bottom = viewport.bottom - options.marginBottom;
         }
 
         var bounds = this.offset();
+
         bounds.right = bounds.left + this.outerWidth();
         bounds.bottom = bounds.top + this.outerHeight();
 
